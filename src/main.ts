326e4f9ea8b664fd257718f2a1b213e3c740f9c1
Maximiliano Vilariño <mvilarino@aldaba.es>
2025-12-22 13:29:42 +0100

    Configuración de variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.port);
+  console.log(`App running on port ${envs.port}`);
 }
 bootstrap();
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -17,8 +17,7 @@ export class ProductsController {
 
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
-    //return this.productsService.create(createProductDto);
-    return createProductDto;
+    return this.productsService.create(createProductDto);
   }
 
   @Get()
